4a5520b3b674a6c675d508c118ba87a70796508a
panfeng <panfeng@codemao.cn>
2019-08-10 19:30:48 +0800

    nothing

--- run_test.py
+++ run_test.py
@@ -11,7 +11,7 @@ from utils.ope_run_data import OpeRunData
 def run_test_action(index):
     Setting.UDID = OpeRunData().get_value(f'device_{index}', 'udid')
     Setting.PORT = OpeRunData().get_value(f'device_{index}', 'port')
-    pytest.main(['--alluredir=.report', 'test_case'])
+    pytest.main(['--alluredir=.report', 'test_action/test_get_score.py'])
 
 
 if __name__ == '__main__':
